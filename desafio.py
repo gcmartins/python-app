268aa837685de3c5910ba9e84856ac15c407a1e0
Gustavo Martins <gucmartins@gmail.com>
2020-01-15 22:19:15 -0300

    Refatoração para organização do código

--- desafio.py
+++ desafio.py
@@ -1,43 +1,4 @@
-dicionario = {
-    0: 'zero',
-    1: 'um',
-    2: 'dois',
-    3: 'tres',
-    4: 'quatro',
-    5: 'cinco',
-    6: 'seis',
-    7: 'sete',
-    8: 'oito',
-    9: 'nove',
-    10: 'dez',
-    11: 'onze',
-    12: 'doze',
-    13: 'treze',
-    14: 'quatorze',
-    15: 'quinze',
-    16: 'dezesseis',
-    17: 'dezessete',
-    18: 'dezoito',
-    19: 'dezenove',
-    20: 'vinte',
-    30: 'trinta',
-    40: 'quarenta',
-    50: 'cinquenta',
-    60: 'sessenta',
-    70: 'setenta',
-    80: 'oitenta',
-    90: 'noventa',
-    100: 'cento',
-    200: 'duzentos',
-    300: 'trezentos',
-    400: 'quatrocentos',
-    500: 'quinhentos',
-    600: 'seissentos',
-    700: 'setecentos',
-    800: 'oitocentos',
-    900: 'novecentos'
-}
-
+from utils import dicionario
 
 
 def extenso(numero):
@@ -53,6 +14,10 @@ def extenso(numero):
     if tamanho <= 3:
         return sinal + centena_extenso(numero)
 
+    return milhar_extenso(numero, sinal)
+
+
+def milhar_extenso(numero, sinal):
     milhar = int(numero / 1000)
     resto = numero % 1000
     if milhar == 1:
